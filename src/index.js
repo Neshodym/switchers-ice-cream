8f958be94850b7374ae8b479452ff2527a7e4d53
Neshodym <voinash@gmail.com>
2022-12-20 12:36:50 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 
+import './js/galery';
+
+import './js/header-modal';
+
+
+import './js/sliding-menu';
+
+
+import './js/contacts-modal';
 
 import './js/locations-modal';
 import './js/franshise-modal';
+
 import './js/about-modal';
-import './js/galery';
+
 import './js/swiper-slider';
 import './js/sliding-menu';
+
 import './js/mobile-menu';
+
+import './js/modal';
+
+
+import './js/modal-prod-menu';
+
+
 import './js/modal-header';
-// import './js/modal-prod-menu';
+
